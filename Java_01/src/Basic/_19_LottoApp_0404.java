c22855cc33a1015262fc5c0dcd87b33b55eb5637
ITWILL <ITWILL@DESKTOP-8J9TG1G>
2023-04-04 16:15:27 +0900

    0404

---
+++ Java_01/src/Basic/_19_LottoApp_0404.java
@@ -0,0 +1,12 @@
+package Basic;
+
+//1~45 범위의 정수형 난수값을 6개 제공받아 출력
+// => 6개의 난수값은 서로 중복되지 않도록하고 오름차순 정렬
+// 정렬(sort) : 오름차순(Ascending sort), 내림차순(Descending sort)
+
+public class _19_LottoApp_0404 {
+	public static void main(String[] args) {
+		
+	}
+
+}
