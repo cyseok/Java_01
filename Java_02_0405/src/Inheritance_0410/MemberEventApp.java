b45eeb1131a398f2f70fe331d1cade9039aaae7d
ITWILL <ITWILL@DESKTOP-8J9TG1G>
2023-04-10 16:13:55 +0900

    0410

--- Java_02_0405/src/Inheritance_0410/MemberEventApp.java
+++ Java_02_0405/src/Inheritance_0410/MemberEventApp.java
@@ -5,7 +5,12 @@ public class MemberEventApp {
 		
 		// 자식 클래스의 생성자로 객체를 생성할 경우 부모클래스의 생성자가 먼저 호출되어
 		// 부모 클래스의 객체가 먼저 생성된 후 자식클래스의 생성자로 객체를 생성하여 상속관계가 성립된다.
-		MemberEvent member1 = new MemberEvent();
+		// 생성자로 객체를 생성하여 상속관계 자동으로 성립
+		// => 자식클래스의 참조변수에는 자식클래스 객체의 메모리 주소를 저장하여 자식클래스 객체의
+		// 필드와 메소드를 참조하지만 상속관계에 의해 부모클래스 객체의 필드 또는 메소드를 참조가능하다.
+		
+		MemberEvent member1 = new MemberEvent();   // 기본생성자 호출
+		
 		
 		member1.setId("abc123");
 		member1.setName("홍길동");
