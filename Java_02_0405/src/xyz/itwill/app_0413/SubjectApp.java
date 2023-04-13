718cda9be79a0b5c78e3235a4447cb109ce85534
ITWILL <ITWILL@DESKTOP-8J9TG1G>
2023-04-13 12:42:49 +0900

    0413

---
+++ Java_02_0405/src/xyz/itwill/app_0413/SubjectApp.java
@@ -0,0 +1,9 @@
+package xyz.itwill.app_0413;
+
+// 패키지(package) : 같은 목적의 자료형(참조형)을 명확하게 구분하여 그룹화 하기 위해 사용
+// Java 참조형 : Class, Interface, Enum(열거형)
+// Java 자료형을 보다 쉽게 관리하기 위해 패키지를 사용한다.
+
+public class SubjectApp {
+
+}
