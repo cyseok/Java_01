f9cb65eaa76454bae7f90851d91054ca9385e493
ITWILL <ITWILL@DESKTOP-8J9TG1G>
2023-04-13 13:00:07 +0900

    0413

--- Java_02_0405/src/xyz/uniwill/subject_0413/JavaSubject.java
+++ Java_02_0405/src/xyz/uniwill/subject_0413/JavaSubject.java
@@ -2,7 +2,7 @@ package xyz.uniwill.subject_0413;
 
 public class JavaSubject {
 	public void display () {
-		System.out.println("아이티윌 학원의 Java 과정입니다.");
+		System.out.println("유니윌 학원의 Java 과정입니다.");
 		
 	}
 
