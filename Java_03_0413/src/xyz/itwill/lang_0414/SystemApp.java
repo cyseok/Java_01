815e9104f3401046a4f79296e5bec23b46e1cf5b
ITWILL <ITWILL@DESKTOP-8J9TG1G>
2023-04-14 13:11:39 +0900

    0414

---
+++ Java_03_0413/src/xyz/itwill/lang_0414/SystemApp.java
@@ -0,0 +1,11 @@
+package xyz.itwill.lang_0414;
+
+// Java API(Application Prigramming Interface) : 
+//          Java 프로그램 개발에 프로그램 기능을 제공 하기 위한 도구
+// => 라이브러리를 이용하여 배포된 Java 자료형(클래스, 인터페이스, 열거형)
+
+
+
+public class SystemApp {
+
+}
