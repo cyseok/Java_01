3924c9a1bbd5c51cb1c162b6a0d49523281d30d3
ITWILL <ITWILL@DESKTOP-8J9TG1G>
2023-04-19 16:02:59 +0900

    0419

--- Java_03_0413/src/_04_xyz/itwill/utill_0419/SimpleDateFormatApp.java
+++ Java_03_0413/src/_04_xyz/itwill/utill_0419/SimpleDateFormatApp.java
@@ -14,8 +14,18 @@ public class SimpleDateFormatApp {
 		// SimpleDateFormat 클래스로 객체를 생성하기 위한 변수에 Date에 대한 패턴정보를 이용
 		// => 객체를 생성하기 위해 생성자를 사용
 		// => 패턴정보 : 패턴문자(날짜와 시간을 임의의 문자로 표현)를 사용하여 표현된 문자열
-		// => 패턴문자 : y(년), M(월), d(일), E(요일), h(12시간), H(24시간), m(분), s(초)
-		SimpleDateFormat dateFormat = new SimpleDateFormat();
+		// => 패턴문자 : y(년), M(월), d(일), E(요일), h(12시간), H(24시간), AM or PM,  m(분), s(초)
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy년 MM월 dd일 E요일");
+		
+		// dateFormat.format() : Date객체에 저장된 정보를 SimpleDateFormat의 패턴정도를 사용해 문자열로 반환
+		String printDate = dateFormat.format(now);
+		
+		System.out.println("현재 날짜 = " + printDate);
+		
+		// 패턴정보를 변경
+		dateFormat.applyPattern("yyyy-MM-dd HH:mm:ss");
+		System.out.println("현재 = " + dateFormat.format(now));
+		
 		
 		
 	}
