2f2de9ee3884fc6d0792c024ba2378108f7d0857
ITWILL <ITWILL@DESKTOP-8J9TG1G>
2023-04-24 12:10:10 +0900

    0424

--- Java_03_0413/src/_05_xyz/itwill/awt_0421/WindowAdapterApp.java
+++ Java_03_0413/src/_05_xyz/itwill/awt_0421/WindowAdapterApp.java
@@ -15,7 +15,13 @@ public class WindowAdapterApp extends Frame {
 	public WindowAdapterApp(String title) {
 		super(title);
 		
-		addWindowListener(new WindowEventHandleTwo());
+		/// addWindowListener(new WindowEventHandleTwo());
+		addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
+			
+		});
 		
 		setBounds(800, 200, 300, 300);
 		setVisible(true);
@@ -29,13 +35,26 @@ public class WindowAdapterApp extends Frame {
 	}
 	
 	// ★ Adapter클래스를 상속받은 자식클래스는 필요한 메소드만 오버라이드 선언하면된다.
+	// Adapter클래스 : Listener 인터페이스 중 추상메소드가 2개이상 선언된경우 listener 인터페이스 대신
+	//                이벤트 처리를 위해 제공되는 클래스
 	
+	
+	/*
 	public class WindowEventHandleTwo extends WindowAdapter {
 		@Override
 		public void windowClosing(WindowEvent e) {
 			System.exit(0);
 		}
-		
 	}
+	*/
+	
+	/* 바로 위 처럼 작성하는 것 대신에 Annonynous로
+	addWindowListener(new WindowAdapter() {
+		public void windowClosing(WindowEvent e) {
+			System.exit(0);
+		}
+		
+	});
+	이렇게 작성해도 됨. */
 
 }
