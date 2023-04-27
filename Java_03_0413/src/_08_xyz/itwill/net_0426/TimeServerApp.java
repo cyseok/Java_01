04d4a58fc6dba4fc81a669e63bb5b7c3d0d94ae5
ITWILL <ITWILL@DESKTOP-8J9TG1G>
2023-04-27 12:57:21 +0900

    0427

--- Java_03_0413/src/_08_xyz/itwill/net_0426/TimeServerApp.java
+++ Java_03_0413/src/_08_xyz/itwill/net_0426/TimeServerApp.java
@@ -1,8 +1,12 @@
 package _08_xyz.itwill.net_0426;
 
 import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Date;
+import java.util.stream.Stream;
 
 // NTP(Network Time Protocol) Server : 날짜와 시간을 제공하는 서버 컴퓨터
 
@@ -30,10 +34,40 @@ public class TimeServerApp {
 				 //     클라이언트의 소켓과 연결될 소켓을 생성해 반환하고 스레드 재실행 
 				 Socket socket = ntpServer.accept();
 				 
-				 System.out.println("socket = " + socket);
+				// System.out.println("socket = " + socket);
+				 
+				 //======================================================================================
+				 /*
+				 //-------------------------------------------------------
+				 // Socket.getOutputStream() : 소켓 객체에 저장된 출력스트림(OutputStream)을 반환하는 메소드
+				 OutputStream out = socket.getOutputStream();
+				 
+				 // OutputStream 객체를 전달받아 객체를 전달할 수 있는 출력스트림으로 확장한 것
+				 ObjectOutputStream out2 = new ObjectOutputStream(out);
+				 
+				 // 출력스트림을 이용하여 시스템의 현재 날짜와 시간이 저장된 Date 객체를 생성하여 전달
+				 // => 클라이언트에게 날짜와 시간정보를 전송해줌.
+				 out2.writeObject(new Date());
+				 */
+				 
+				 //-------------------------------------------------------
+				 
+				 // 위 3줄의 식별자 대신에 한번에 작성하면 좋다
+				 new ObjectOutputStream(socket.getOutputStream()).writeObject(new Date());
+				 
+				 
+				 //======================================================================================
+				 
+				 // 로그 처리 -> 기록
+				 // Socket.getInetAddress() : 서버의 소켓과 연결된 외부 컴퓨터의 네트워크 식별자가 저장된 
+				 //                            InetAddress객체를 반환하는 메소드
+				 System.out.println("정보) 클라이언트[" + socket.getInetAddress().getHostAddress() + "]에게 날짜와 시간을 제공");
+				 
+				 // 클라이언트와의 접속 해체
+				 socket.close();
 			 }
 		 } catch (IOException e) {
-			// TODO: handle exception
+			System.out.println("error) 서버 네트워크에 문제가 발생되었습니다.");
 		}
 		 
 		 
