38c642833e581c9c4b1259cda1bc8c8d9c89f9df
user <user@host.docker.internal>
2023-05-15 11:14:35 +0900

    0515

--- jdbc/src/_01_xyz/itwill/jdbc_05_11/DeleteStudentApp.java
+++ jdbc/src/_01_xyz/itwill/jdbc_05_11/DeleteStudentApp.java
@@ -1,10 +1,51 @@
 package _01_xyz.itwill.jdbc_05_11;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 //STUDENT 테이블에 저장된 학생정보 중 학번이 [3000]인 학생정보를 삭제하는 JDBC 프로그램 작성
 
 
 public class DeleteStudentApp {
 	public static void main(String[] args) {
+		
+		Connection con = null;
+		Statement stmt = null;
+		
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			
+			String url = "jdbc:oracle:thin:@localhost:1521:xe";
+			String user = "scott";
+			String password = "tiger";
+			con = DriverManager.getConnection(url, user, password);
+			
+			stmt = con.createStatement();
+			
+			String sql = "delete from sudent where no = 3000";
+			int rows = stmt.executeUpdate(sql);
+			
+			if(rows > 0) {
+				System.out.println("메시지) " + rows + "명의 학생정보를 삭제하였습니다.");
+				
+			} else {
+				System.out.println("메시지) 학생정보를 찾을 수 없습니다.");
+			}
+					
+		} catch (ClassNotFoundException e) {
+			System.out.println("메시지) OracleDriver 클래스를 찾을 수 없습니다.");
+			
+		} catch(SQLException e) {
+			System.out.println("Error) JDBC관련 오류 = " + e.getMessage());
+		} finally {
+			try {
+				if(stmt != null) stmt.close();
+				if(con != null) con.close();
+				
+			} catch(SQLException e) {}
+		}
 		
 	}
 
--- jdbc/src/_01_xyz/itwill/jdbc_05_11/TransactionControlApp.java
+++ jdbc/src/_01_xyz/itwill/jdbc_05_11/TransactionControlApp.java
@@ -5,14 +5,13 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-//JDBC 프로그램은 기본적으로 AutoCommit 기능이 활성화 되어있어 SQL 명령(DML)이 전달되면
-//실행되어 자동으로 커밋 처리
-//=> 프로그램 실행시 예외가 발생된 경우 예외 발생전 전달되어 실행된 SQL 명령에 대한 롤백 처리 불가능
-//JDBC 프로그램에서 AutoCommit 기능을 비활성화 처리하고 예외 발생 없이 프로그램이 정상적으로
-//실행되면 커밋 처리하고 예외가 발생된 경우 롤백 처리하는 것을 권장
+// JDBC 프로그램은 기본적으로 AutoCommit 기능이 활성화 되어있어 SQL 명령(DML)이 전달되면
+// 실행되어 자동으로 커밋 처리
+// => 프로그램 실행시 예외가 발생된 경우 예외 발생전 전달되어 실행된 SQL 명령에 대한 롤백 처리 불가능
+// JDBC 프로그램에서 AutoCommit 기능을 비활성화 처리하고 예외 발생 없이 프로그램이 정상적으로
+// 실행되면 커밋 처리하고 예외가 발생된 경우 롤백 처리하는 것을 권장
 
-//STUDENT 테이블에 저장된 학생정보 중 학번이 [2000]인 학생의 이름을 [임꺽정]으로 변경하는 JDBC 프로그램 작성
-
+// STUDENT 테이블에 저장된 학생정보 중 학번이 [2000]인 학생의 이름을 [임꺽정]으로 변경하는 JDBC 프로그램 작성
 
 public class TransactionControlApp {
 	
@@ -29,7 +28,7 @@ public class TransactionControlApp {
 			String password="tiger";
 			con=DriverManager.getConnection(url, user, password);
 
-			//Connection.setAutoCommit(boolean autoCommit) : AutoCommit 기능의 사용유무를 변경하는 메소드
+			// Connection.setAutoCommit(boolean autoCommit) : AutoCommit 기능의 사용유무를 변경하는 메소드
 			// => false : AutoCommit 기능 비활성화, true : AutoCommit 기능 활성화 - 기본값
 			con.setAutoCommit(false);
 			
@@ -38,20 +37,20 @@ public class TransactionControlApp {
 			String sql="update student set name='임꺽정' where no=2000";
 			int rows=stmt.executeUpdate(sql);
 			
-			//if(con!=null) throw new Exception();//인위적 예외 발생
+			// if(con!=null) throw new Exception();//인위적 예외 발생
 			
-			if(rows > 0) {//조작행이 있는 경우
+			if(rows > 0) {  // 조작행이 있는 경우
 				System.out.println("[메세지]"+rows+"명의 학생정보를 변경 하였습니다.");
-			} else {//조작행이 없는 경우
+			} else {  // 조작행이 없는 경우
 				System.out.println("[메세지]변경 처리할 학번의 학생정보를 찾을 수 없습니다.");
 			}
 			
-			//Connection.commit() : 커밋 처리하는 메소드 - 전달된 SQL 명령이 실제 테이블에 적용
+			// Connection.commit() : 커밋 처리하는 메소드 - 전달된 SQL 명령이 실제 테이블에 적용
 			con.commit();
 		} catch (Exception e) {
 			System.out.println("[에러]프로그램에 예기치 못한 오류가 발생 되었습니다.");
 			try {
-				//Connection.rollback() : 롤백 처리하는 메소드 - 전달된 SQL 명령의 실행을 취소 
+				// Connection.rollback() : 롤백 처리하는 메소드 - 전달된 SQL 명령의 실행을 취소 
 				con.rollback();
 			} catch (SQLException e1) { }
 		}  finally {
