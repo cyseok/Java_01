1cd947261cba21e85b36f432c660c3e99eaaa458
user <user@host.docker.internal>
2023-08-07 19:06:55 +0900

    0807

---
+++ spring/src/main/java/xyz/itwill10/controller/JoinController.java
@@ -0,0 +1,136 @@
+package xyz.itwill10.controller;
+//
+import java.util.Map;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+//
+@Controller
+public class JoinController {
+	@RequestMapping(value = "join", method = RequestMethod.GET)
+	public String join() {
+		return "join_form";   /// join_form 응답 요청
+	}
+	/*
+	//전달값을 받아다가 매개변수에 저장
+	//전달값을 제공받아 Request Scope 속성값으로 저장하고 속성값을 출력하기 위한 JSP 문서의 뷰이름을 반환하는 요청 처리 메소드
+	// => 전달값과 같은 이름의 매개변수에 전달값을 제공받아 요청 처리 메소드에서 사용
+	// => @RequestParam 어노테이션을 사용하여 매개변수와 같은 이름으로 전달되는 값이 없는 경우 400 에러코드 발생
+	@RequestMapping(value = "join", method = RequestMethod.POST)
+	public String join(@RequestParam String id, @RequestParam String passwd
+			, @RequestParam String name, @RequestParam String email, Model model) {
+		model.addAttribute("id", id);
+		model.addAttribute("passwd", passwd);
+		model.addAttribute("name", name);
+		model.addAttribute("email", email);
+		return "join_display";
+	}
+	*/
+	//=======================================================================================================
+	/*
+	//전달값을 제공받아 Request Scope 속성값으로 저장하고 속성값을 출력하기 위한 JSP 문서의 뷰이름을 반환하는 요청 처리 메소드
+	// => 전달값과 같은 이름의 매개변수에 전달값을 제공받아 요청 처리 메소드에서 사용
+	// => @ModelAttribute 어노테이션을 사용하여 매개변수에 저장된 값을 속성값으로 저장하여 뷰(View) 제공
+	//@ModelAttribute : 객체(값)을 뷰에게 제공하기 위한 어노테이션
+	// => 메소드에 @ModelAttribute 어노테이션을 사용하면 메소드의 반환값을 속성값으로 저장하여 요청 처리 클래스의 모든 요청 처리 메소드의 뷰에게 제공
+	// => 매개변수에 @ModelAttribute 어노테이션을 사용하면 매개변수에 저장된 값을 속성값으로 저장하여 요청 처리 메소드의 뷰에게 제공
+	//value 속성 : 뷰(JSP)에서 속성값을 사용하기 위한 속성명을 속성값으로 설정
+	// => 다른 속성이 없는 경우 속성값만 설정 가능
+	//@ModelAttribute 어노테이션을 사용하면 매개변수와 같은 이름의 전달값이 없어도 400 에러코드 미발생
+	@RequestMapping(value = "join", method = RequestMethod.POST)
+	public String join(@ModelAttribute(value = "id") String id, @ModelAttribute("passwd") String passwd
+			, @ModelAttribute("name") String name, @ModelAttribute("email") String email) {
+		return "join_display";
+	}
+	=======================================================================================================
+	*/
+	
+	/* VO 클래스 생성해서 메소드 작성
+	//전달값을 매개변수로 제공받아 객체의 필드값으로 저장하고 Request Scope 속성값으로 저장하고 속성값을 출력하기 위한 JSP 문서의 뷰이름을 반환하는 요청 처리 메소드
+	@RequestMapping(value = "join", method = RequestMethod.POST)
+	public String join(@RequestParam String id, @RequestParam String passwd
+			, @RequestParam String name, @RequestParam String email, Model model) {
+		Member member=new Member();
+		member.setId(id);
+		member.setName(name);;
+		member.setName(name);
+		member.setEmail(email);
+		//
+		model.addAttribute("member", member);
+		//
+		return "join_display";
+	}
+	=======================================================================================================
+	*/
+	
+	/*
+	@RequestMapping(value = "join", method = RequestMethod.POST)
+	public String join(@RequestParam String id, @RequestParam String passwd
+			, @RequestParam String name, @RequestParam String email, Model model) {
+		Member member=new Member();
+		member.setId(id);
+		member.setName(name);;
+		member.setName(name);
+		member.setEmail(email);
+		//
+		// model.addAttribute("member", member);
+		// Modle.addAttribute(Object attributeValue) : Model 객체에 속성명과 속성값(객체)을 저장하기 위한 메소드 - Request Scope
+		// => 속성값의 자료형(클래스)을 속성명으로 자동 설정 - 첫문자는 소문자
+		// => 속성값으로 원시값(Wrapper 객체) 또는 String 객체 저장 불가
+		model.addAttribute(member);
+		//
+		return "join_display";
+	}
+	=======================================================================================================
+	*/
+	
+	/*
+	//★요청 페이지의 전달값이 있는 경우 요청 처리 메소드의 매개변수 자료형을 VO 클래스(DTO 클래스)로 작성하면 
+	//	Front Controller는 VO 클래스를 객체로 생성하여 매개변수에 전달하여 저장
+	// => VO 클래스의 객체 필드에는 전달값의 이름과 같은 필드에 전달값을 제공받아 저장
+	// Command 객체 : 전달값을 제공받아 필드에 저장된 객체로 요청 처리 메소드의 뷰에게 속성값으로 저장하여 사용될 수 있도록 제공
+	// => @ModelAttribute 어노테이션을 사용하여 객체를 속성값으로 저장해 뷰에게 제공
+	// => @ModelAttribute 어노테이션 생략 가능
+	// => value 속성을 생략하면 Command 객체의 자료형(클래스)이 속성명으로 자동 설정
+	@RequestMapping(value = "join", method = RequestMethod.POST)
+	public String join(@ModelAttribute Member member) {
+		return "join_display";
+	}
+	=======================================================================================================
+	*/
+	
+	/*
+	//@ModelAttribute 어노테이션의 value 속성을 사용하여 뷰에게 제공될 속성값의 속성명을 변경 가능
+	@RequestMapping(value = "join", method = RequestMethod.POST)
+	public String join(@ModelAttribute(value = "mem") Member member, Model model) {
+		if(member.getId().equals("asd123")) {//아이디가 중복된 경우
+			model.addAttribute("message", "이미 사용중인 아이디를 입력 하였습니다.");
+			return "join_form";//입력페이지 이동
+		}
+		return "join_display";
+	}
+	*/
+	// ★요청 페이지의 전달값이 있는 경우 요청 처리 메소드의 매개변수 자료형으 Map 인터페이스로 작성하면 Front Controller는 Map 객체를 생성하여 매개변수에 전달하여 저장
+	// => Map 객체는 전달값의 이름을 맵키(MapKey - String)로 설정하여 전달값을 맵값(MapValue)로 제공받아 엔트리로 추가
+	// => Map 인터페이스로 작성된 매개변수에는 반드시 @RequestParam 어노테이션을 사용해야만 Map 객체의 맵값으로 전달값을 제공받아 사용 가능
+	@RequestMapping(value = "join", method = RequestMethod.POST)
+	public String join(@RequestParam Map<String, Object> map, Model model) {
+		model.addAttribute("mem", model);
+		return "join_display";
+	}
+	
+	@RequestMapping(value = "/same_param", method = RequestMethod.GET)
+	public String input() {
+		return "same_form";
+	}
+	
+	//같은 이름으로 전달된 값이 있는 경우 배열을 저장할 수 있는 매개변수를 작성하여 전달값을
+	//문자열 배열로 제공받아 저장
+	@RequestMapping(value = "/same_param", method = RequestMethod.POST)
+	public String input(@RequestParam String[] food, Model model) {
+		model.addAttribute("food", food);
+		return "same_display";
+	}
+}
---
+++ spring/src/main/java/xyz/itwill10/controller/RedirectController.java
@@ -0,0 +1,61 @@
+package xyz.itwill10.controller;
+//
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+//
+@Controller
+public class RedirectController {
+	@RequestMapping("/forward_move")
+	public String forward(Model model) {
+		// Model 객체를 이용하여 뷰에게 제공할 객체를 속성값으로 저장 - Request Scope
+		model.addAttribute("name", "홍길동");
+		// 뷰를 생성하기 위한 뷰이름(ViewName) 반환
+		// => Front Controller는 제공받은 뷰이름을 InternalResourceViewResolver 객체를 사용하여 JSP 문서로 변환하고 JSP 문서로 포워드 이동하여 응답 처리
+		// 포워드 이동 : 서버측에서 현재 웹프로그램에서 다른 웹프로그램으로 스레드를 이동하여 명령을 실행
+		//→ 클라이언트의 요청 URL 주소는 변경되지 않으며 Request Scope 속성값을 객체로 제공받아 사용 가능
+		return "display_forward";
+	}
+	
+	/*
+	@RequestMapping("/redirect_move")
+	public String redirect(Model model) {
+		model.addAttribute("name", "임꺽정");
+		return "display_redirect";
+	}
+	
+	@RequestMapping("/redirect")
+	public String redirect() {
+		// ★뷰이름에 redirect 접두사를 사용하여 URL 주소를 반환하면 Front Controller는 반환받은 뷰이름의 URL 주소를 클라이언트에게 전달 - 리다이렉트 이동 처리
+		// => 클라이언트에게 URL 주소를 전달하여 응답 처리하면 클라이언트는 해당 URL 주소로 브라우저의 주소를 변경하고 서버에 URL 주소의 페이지 요청 처리
+		// 리다이렉트 이동 : 클라이언트에게 URL 주소를 전달하여 페이지를 재요청하여 웹프로그램의 명령 실행
+		// => 클라이언트의 요청 URL 주소 변경되지만, Request Scope 속성값 사용 불가능
+		return "redirect:/redirect_move";
+	}
+	*/
+	
+	@RequestMapping("/redirect_move")
+	public String redirect() {
+		return "display_redirect";
+	}
+	
+	/*사용 불가능
+	@RequestMapping("/redirect")
+	public String redirect(Model model) {
+		//Model 객체에 저장된 속성값은 리다이렉트 이동된 페이지의 요청 처리 메소드 및 뷰에서 사용 불가능
+		// => Model 객체로 저장된 속성값이 문자열인 경우 질의문자열 형식으로 요청 페이지에 값 전달
+		model.addAttribute("name", "임꺽정");
+		return "redirect:/redirect_move";
+	}
+	*/
+	//사용 불가능 → 사용 가능으로 바꾸는 방법
+	//RedirectAttributes 객체 : 리다이렉트 이동되는 페이지의 요청 처리 메소드와 뷰에게 Request Scope 속성값을 저장하여 사용하기 위한 기능을 제공하는 객체
+	@RequestMapping("/redirect")
+	public String redirect(RedirectAttributes attributes) {
+		//RedirectAttributes.addFlashAttribute(String attributeName, Object attributeValue)
+		// => 속성명과 속성값을 저장하여 리다이렉트 이동되는 페이지의 요청 처리 메소드와 뷰에게 속성값을 제공하기 위한 메소드 - Request Scope
+		attributes.addFlashAttribute("name", "임꺽정");
+		return "redirect:/redirect_move";
+	}
+}
